bf0ce688a51022f96295040b0c8fe02d531eb27f
Uday <udaysharma4600@gmail.com>
2024-09-03 20:10:30 +0530

    Frontend Real State FreeLance Project

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -91,10 +91,9 @@ export const Navbar = () => {
           <DisclosurePanel className="sm:hidden">
             <div className="space-y-1 px-2 pb-3 pt-2">
               {navigation.map((item) => (
-                <DisclosureButton
+                <NavLink
                   key={item.name}
-                  as="a"
-                  href={item.href}
+                  to={item.href}
                   aria-current={item.current ? 'page' : undefined}
                   className={classNames(
                     item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
@@ -102,7 +101,7 @@ export const Navbar = () => {
                   )}
                 >
                   {item.name}
-                </DisclosureButton>
+                </NavLink>
               ))}
             </div>
           </DisclosurePanel>
